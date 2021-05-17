35fd16642ba744d752624c6b557fd6a6635bb52c
mob <marc-olivier.bloch@elca.ch>
2021-05-17 12:26:54 +0200

    Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
-import { faFlagUsa, faFlag } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +6,6 @@ import { faFlagUsa, faFlag } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  faFlagUsa = faFlagUsa;
-  faFlag = faFlag;
-
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'fr']);
-    translate.setDefaultLang('en');
-
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  constructor() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,46 +7,26 @@ import { MatMenuModule } from '@angular/material/menu';
 import { AppComponent } from './app.component';
 import { TextAdventureComponent } from './text-adventure/text-adventure.component';
 import { ItemTestComponent } from './item-test/item-test.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DebugComponent } from './debug/debug.component';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
-}
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextAdventureComponent,
     ItemTestComponent,
-    DebugComponent,
   ],
   imports: [
     RouterModule.forRoot([
       { path: 'item-test', component: ItemTestComponent },
       { path: 'text-adventure', component: TextAdventureComponent },
-      { path: 'debug', component: DebugComponent },
-      { path: '', redirectTo: 'debug', pathMatch: 'full' }
+      { path: '', redirectTo: 'text-adventure', pathMatch: 'full' }
     ]),
     BrowserModule,
     BrowserAnimationsModule,
     MatMenuModule,
     MatIconModule,
-    FontAwesomeModule,
-    NgbModule,
     BrowserModule,
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
