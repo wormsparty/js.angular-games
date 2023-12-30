7c0437c21891efceb20b0ac43be859079daed74f
wormsparty <wormsparty@gmail.com>
2023-12-30 05:09:45 +0100

    Fix name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'js-new' title`, () => {
+  it(`should have the 'Angular-games' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('js-new');
+    expect(app.title).toEqual('Angular-games');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, js-new');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-games');
   });
 });
